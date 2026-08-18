b771e59efe7938c7ce3ebf0710680401c4f3b16e
Sanket Kumar Kar <sanket@example.com>
2026-08-18 22:30:00 +0530

    fix: like and tweet after testing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -25,16 +25,7 @@ const generateAccessAndRefereshTokens = async (userId) => {
 };
 
 const registerUser = asyncHandler(async (req, res) => {
-    // get user details from frontend
-    // validation - not empty
-    // check if user already exists: username, email
-    // check for images, check for avatar
-    // upload them to cloudinary, avatar
-    // create user object - create entry in db
-    // remove password and refresh token field from response
-    // check for user creation
-    // return res
-
+  
     const { fullName, email, username, password } = req.body;
     //console.log("email: ", email);
 
@@ -99,6 +90,7 @@ const registerUser = asyncHandler(async (req, res) => {
         );
     }
 
+    
     return res
         .status(201)
         .json(
